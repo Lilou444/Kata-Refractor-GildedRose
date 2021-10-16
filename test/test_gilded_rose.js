f0525967bcd73edeb732855f3dcf4472af23fc03
Sylvie <ng.sylvie@hotmail.com>
2021-10-16 16:27:22 +0200

    UT : Aged Brie

--- test/test_gilded_rose.js
+++ test/test_gilded_rose.js
@@ -5,8 +5,32 @@ describe("Gilded Rose", function() {
   it("should conjured is ok", function() {
     const gildedRose = new Shop([ new Item("Conjured Mana Cake", 0, 6) ]);
     const items = gildedRose.updateQuality();
-    console.log(items);
     expect(items[0].quality).to.equal(2);
   });
+
+  it("should aged brie is ok when sellIn is greater than 0", function() {
+    const gildedRose = new Shop([ new Item("Aged Brie", 2, 0) ]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).to.equal(1);
+  });
+
+  it("should aged brie is not ok when sellIn is greater than 0", function() {
+    const gildedRose = new Shop([ new Item("Aged Brie", 2, 0) ]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).to.equal(2);
+  });
+
+  it("should aged brie is ok when sellIn is less than 0 ", function() {
+    const gildedRose = new Shop([ new Item("Aged Brie", 0, 10) ]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).to.equal(12);
+  });
+
+  it("should aged brie is not ok when sellIn is less than 0 ", function() {
+    const gildedRose = new Shop([ new Item("Aged Brie", 0, 10) ]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).to.equal(11);
+  });
+  
 });
 
